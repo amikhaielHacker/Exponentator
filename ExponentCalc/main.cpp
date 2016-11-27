339211c10900fa703c39f9d1b2b5c25fc2edbe6d
amikhaielYoungDev <amikhaiel@protonmail.ch>
2016-11-27 19:33:13 +0100

    Initial Commit

---
+++ ExponentCalc/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  ExponentCalc
+//
+//  Created by adi on 27.11.16.
+//  Copyright © 2016 AmikhaielHacker. All rights reserved.
+//
+
+#include <iostream>
+#include <math.h> //I imported this for use the 'pow' function
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    float base;
+    float exponent;
+    cout << "Enter the base number in decimal value..." << endl;
+    cin >> base;
+    cout << "Enter the exponent number in decimal value..." <<endl;
+    cin >> exponent;
+    cout << "The result is " << pow(base, exponent)<<endl; //Will be printed to the console the result.
+    
+}
